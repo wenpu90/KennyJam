74a37f6dc88b79262850cfb748db956405db58ce
lonnrek <tehkokthong96@gmail.com>
2019-08-10 16:11:53 +0800

    Movement Done

---
+++ KennyJam/Assets/Lonnrek/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float speed = 5;
+
+
+    private void Update()
+    {
+        Move();
+    }
+    public void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 vec3 = new Vector3(horizontal, 0, vertical);
+        transform.Translate(vec3 * speed * Time.deltaTime);
+    }
+}
